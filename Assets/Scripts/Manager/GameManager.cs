7cb3300a5a913ab45673f4ca8c338263612e328a
tanku3080 <57029371+tanku3080@users.noreply.github.com>
2020-12-25 01:36:52 +0900

    タイトルの画像を設置。GameManagerとシーン切り替えのスクリプトを追加

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int SectionNumber = 0;
+    public bool OnChangeScene = false;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+    }
+}
